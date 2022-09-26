ad2b065afb916b46fd17dd11138c2b1dcd443842
gurcan16 <108872652+gurcan16@users.noreply.github.com>
2022-09-26 17:09:06 +0300

    Add project files.

---
+++ kampOdevi/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kampOdevi
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
---
+++ kampOdevi/Program.cs
@@ -0,0 +1,43 @@
+﻿using kampOdevi;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product product1 = new Product();
+        product1.Id = 1;
+        product1.Name = "Mont";
+        product1.Price = 495.98;
+
+        Product product2 = new Product();
+        product2.Id = 2;
+        product2.Name = "Sweatshirt";
+        product2.Price = 259.99;
+
+        Product product3 = new Product();
+        product3.Id = 3;
+        product3.Name = "T shirt";
+        product3.Price = 109.95;
+
+        Product[] products = new Product[] { product1, product2, product3 };
+
+        for (int i = 0; i < products.Length; i++)
+        {
+            Console.WriteLine("ID:" + products[i].Id + " " + "Ürünün Adı:" + products[i].Name + " " + "Fiyatı:" + products[i].Price);
+        }
+        Console.WriteLine(" ");
+        foreach (var product in products)
+        {
+            Console.WriteLine("ID:" + product.Id + " " + "Ürünün Adı:" + product.Name + " " + "Fiyatı " + product.Price);
+        }
+        Console.WriteLine(" ");
+        int a = 0;
+        while (a <3)
+        {
+            Console.WriteLine("ID:" + products[a].Id + " " + "Ürünün Adı:" + products[a].Name + " " + "Fiyatı " + products[a].Price);
+            a++;
+            
+        }
+    }
+}
